b54e5c319c556530aa31693107e96001fdaeb9fb
lai110771 <lai0919480820@gamil.com>
2023-12-05 21:49:04 +0100

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,19 @@
 export const cart = [];
+
+export function addToCart(productId){
+    let matchingItem;
+    cart.forEach((carItem)=>{
+        if (productId === carItem.productId){
+            matchingItem = carItem;
+        }
+    })
+    if(matchingItem){
+        matchingItem.quantity++;
+    }
+    else{
+        cart.push({
+            productId: productId,
+            quantity: 1
+        })
+    }
+}
